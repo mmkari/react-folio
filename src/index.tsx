2f2b75aa990aaf2c80fb51673c6517655d98aee7
Miko Kari <miko.kari@outlook.com>
2020-12-21 23:40:02 +0200

    added module exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
 export { default, default as Paged } from './Paged';
+export {
+  default as withDimensions,
+  DimensionContextProvider,
+  DimensionContextConsumer,
+  BreakingBlock,
+} from './DimensionContext';
+export { default as Text } from './Text';
